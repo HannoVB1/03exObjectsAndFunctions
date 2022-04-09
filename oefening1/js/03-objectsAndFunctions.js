97546511d0d166ff347ff53d616627bd6ec35a0a
HannoVB1 <hannovb@hotmail.be>
2022-04-09 23:05:16 +0200

    Stuck at change h1

--- oefening1/js/03-objectsAndFunctions.js
+++ oefening1/js/03-objectsAndFunctions.js
@@ -4,14 +4,13 @@ const myBicycle = {
   speed: 30,
   gear: 1,
   frameMaterial: 'carbon fibre',
-  accelerate: function(perc) {myBicycle.speed += myBicycle.speed * (perc /100)}
 };
 
 // Voeg een methode accelerate toe aan myBicycle
 // De methode heeft 1 parameter, een percentage waarmee
 // de speed van myBicycle moet worden aangepast
 
-
+myBicycle.accelerate = function(perc) {myBicycle.speed += myBicycle.speed * (perc /100)};
 
 // Laat de fiets 25% versnellen. Log de speed voor en na
 // de methode aanroep.
@@ -25,7 +24,18 @@ console.log(myBicycle.speed);
 // speed [20..40[ 'Pushing...'
 // speed [40..60[ 'Peleton style...'
 // speed >= 60 'Sagan style...
-//
+
+myBicycle.speed = function(speed)
+  {let result;
+    (speed>=0 && speed<10) ? result = "Walking... ":
+    (speed>=10 && speed<20) ? result = "Brakes on... ":
+    (speed>=20 && speed<40) ? result = "Pushing... ":
+    (speed>=40 && speed<60) ? result = "Peleton style... ":
+    (speed>=60) ? result = "Sagan style... ": result = "error";
+    return result;
+  };
+
+console.log(myBicycle.speed(30));
 // Stel de speed van myBicycle in op 5
 // Test nu bovenstaande methode door in een lusje de speed in stapjes van 10%
 // te verhogen (via de methode accelerate) en telkens de geretourneerde string te loggen.
